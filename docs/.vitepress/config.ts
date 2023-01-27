fd85645ecff84ac46dd06eff04804ebe295de49a
luhaifeng666 <youzui@hotmail.com>
2023-01-27 20:50:12 +0800

    chore: algolia 搜索不到结果,暂时下线

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: luhaifeng666 youzui@hotmail.com
  * @Date: 2022-08-23 13:54:33
  * @LastEditors: luhaifeng666
- * @LastEditTime: 2023-01-18 11:44:13
+ * @LastEditTime: 2023-01-27 20:48:54
  * @Description: 
  */
 import fg from 'fast-glob'
@@ -133,54 +133,11 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/luhaifeng666/obsidian-plugin-docs-zh' }
     ],
-    algolia: {
-      appId: 'VI8LY5RZKO',
-      apiKey: 'e2f1997c9bd194f6b5357ff70e15faf2',
-      indexName: 'dev_obsidian_plugin_docs_zh',
-      // locales: {
-      //   zh: {
-      //     placeholder: '搜索文档',
-      //     translations: {
-      //       button: {
-      //         buttonText: '搜索文档',
-      //         buttonAriaLabel: '搜索文档'
-      //       },
-      //       modal: {
-      //         searchBox: {
-      //           resetButtonTitle: '清除查询条件',
-      //           resetButtonAriaLabel: '清除查询条件',
-      //           cancelButtonText: '取消',
-      //           cancelButtonAriaLabel: '取消'
-      //         },
-      //         startScreen: {
-      //           recentSearchesTitle: '搜索历史',
-      //           noRecentSearchesText: '没有搜索历史',
-      //           saveRecentSearchButtonTitle: '保存至搜索历史',
-      //           removeRecentSearchButtonTitle: '从搜索历史中移除',
-      //           favoriteSearchesTitle: '收藏',
-      //           removeFavoriteSearchButtonTitle: '从收藏中移除'
-      //         },
-      //         errorScreen: {
-      //           titleText: '无法获取结果',
-      //           helpText: '你可能需要检查你的网络连接'
-      //         },
-      //         footer: {
-      //           selectText: '选择',
-      //           navigateText: '切换',
-      //           closeText: '关闭',
-      //           searchByText: '搜索提供者'
-      //         },
-      //         noResultsScreen: {
-      //           noResultsText: '无法找到相关结果',
-      //           suggestedQueryText: '你可以尝试查询',
-      //           reportMissingResultsText: '你认为该查询应该有结果？',
-      //           reportMissingResultsLinkText: '点击反馈'
-      //         }
-      //       }
-      //     }
-      //   }
-      // }
-    },
+    // algolia: {
+    //   appId: 'VI8LY5RZKO',
+    //   apiKey: 'e2f1997c9bd194f6b5357ff70e15faf2',
+    //   indexName: 'dev_obsidian_plugin_docs_zh'
+    // },
     sidebar
   }
 })
